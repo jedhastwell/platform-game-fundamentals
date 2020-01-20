ed6feb736ec35720bfd0e0418af989bc047a4097
Jed Hastwell <jed@whistlefire.com>
2020-01-20 12:59:11 +0100

    Modify horizontal movement to use acceleration

--- src/entities/Hero.js
+++ src/entities/Hero.js
@@ -14,6 +14,8 @@ class Hero extends Phaser.GameObjects.Sprite {
     this.body.setCollideWorldBounds(true);
     this.body.setSize(12, 40);
     this.body.setOffset(12, 23);
+    this.body.setMaxVelocity(250, 400);
+    this.body.setDragX(750);
 
     this.keys = scene.cursorKeys;
   }
@@ -22,15 +24,15 @@ class Hero extends Phaser.GameObjects.Sprite {
     super.preUpdate(time, delta);
     
     if (this.keys.left.isDown) {
-      this.body.setVelocityX(-250);
+      this.body.setAccelerationX(-1000);
       this.setFlipX(true);
       this.body.offset.x = 8;
     } else if (this.keys.right.isDown) {
-      this.body.setVelocityX(250);
+      this.body.setAccelerationX(1000);
       this.setFlipX(false);
       this.body.offset.x = 12;
     } else {
-      this.body.setVelocityX(0);
+      this.body.setAccelerationX(0);
     }
   }
 
