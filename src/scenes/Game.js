23f29f8dd33bb558fcf1dd010e7817826fceb34f
Jed Hastwell <jed@whistlefire.com>
2020-01-20 17:01:27 +0100

    Load spritesheets and create animations

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -8,7 +8,32 @@ class Game extends Phaser.Scene {
   }
 
   preload() {
+    this.load.spritesheet('hero-idle-sheet', 'assets/hero/idle.png', {
+      frameWidth: 32,
+      frameHeight: 64,
+    });
+
     this.load.spritesheet('hero-run-sheet', 'assets/hero/run.png', {
+      frameWidth: 32,
+      frameHeight: 64,
+    });
+
+    this.load.spritesheet('hero-pivot-sheet', 'assets/hero/pivot.png', {
+      frameWidth: 32,
+      frameHeight: 64,
+    });
+
+    this.load.spritesheet('hero-jump-sheet', 'assets/hero/jump.png', {
+      frameWidth: 32,
+      frameHeight: 64,
+    });
+
+    this.load.spritesheet('hero-flip-sheet', 'assets/hero/spinjump.png', {
+      frameWidth: 32,
+      frameHeight: 64,
+    });
+
+    this.load.spritesheet('hero-fall-sheet', 'assets/hero/fall.png', {
       frameWidth: 32,
       frameHeight: 64,
     });
@@ -19,8 +44,39 @@ class Game extends Phaser.Scene {
     this.cursorKeys = this.input.keyboard.createCursorKeys();
     
     this.anims.create({
+      key: 'hero-idle',
+      frames: this.anims.generateFrameNumbers('hero-idle-sheet'),
+    });
+
+    this.anims.create({
       key: 'hero-running',
       frames: this.anims.generateFrameNumbers('hero-run-sheet'),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: 'hero-pivoting',
+      frames: this.anims.generateFrameNumbers('hero-pivot-sheet'),
+    });
+
+    this.anims.create({
+      key: 'hero-jumping',
+      frames: this.anims.generateFrameNumbers('hero-jump-sheet'),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: 'hero-flipping',
+      frames: this.anims.generateFrameNumbers('hero-flip-sheet'),
+      frameRate: 30,
+      repeat: 0,
+    });
+
+    this.anims.create({
+      key: 'hero-falling',
+      frames: this.anims.generateFrameNumbers('hero-fall-sheet'),
       frameRate: 10,
       repeat: -1,
     });
