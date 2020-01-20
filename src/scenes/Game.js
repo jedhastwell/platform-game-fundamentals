1e718d8ef280a0b304d9a9299cfba7cb6c46e071
Jed Hastwell <jed@whistlefire.com>
2020-01-20 12:33:47 +0100

    Load the hero's run spritesheet

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -9,11 +9,14 @@ class Game extends Phaser.Scene {
   init(data) {}
 
   preload() {
-    this.load.image('logo', 'assets/phaser3-logo.png');
+    this.load.spritesheet('hero-run-sheet', 'assets/hero/run.png', {
+      frameWidth: 32,
+      frameHeight: 64,
+    });
   }
 
   create(data) {
-    this.add.image(400, 300, 'logo');
+    this.add.sprite(400, 300, 'hero-run-sheet', 5);
   }
 
   update(time, delta) {}
